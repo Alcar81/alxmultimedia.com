292363a288574fae4b5f546c9e3242d9f8c913d0
Alexandre Carignan <alexandre.carignan@gmail.com>
2024-11-12 17:20:45 -0500

    Retirer le sous-module backend

--- backend/README.md
+++
@@ -1,2 +0,0 @@
-# alx_back
-alx_back
--- backend/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-// C:\projetChatGPT\backend\middleware\errorHandler.js
-
-const errorHandler = (err, req, res, next) => {
-    console.error(err);
-    res.status(err.status || 500).json({ message: err.message || 'Erreur serveur' });
-};
-
-module.exports = errorHandler;
